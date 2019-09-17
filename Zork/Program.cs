8d52bc9efafdeb6e705831dff0f277cd588ddaf5
ewhite755 <54605007+ewhite755@users.noreply.github.com>
2019-09-17 19:21:52 -0400

    Update Program.cs

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,21 +8,42 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string response;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        response = "Thank you for playing!";
+                        break;
+
+                    case Commands.LOOK:
+                        response = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        response = $"You moved {command}.";
+                        break;
+
+                    default:
+                        response = "Unkown command.";
+                        break;
+                }
+
+                Console.WriteLine(response);
+            }
         }
         
         private static  Commands ToCommand(string commandString)
         {
-           if (Enum.TryParse(commandString, true, out Commands result))
-            {
-                return result;
-            }
-           else
-            {
-                return Commands.UNKNOWN;
-            }
+            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
         }
     }
 }
