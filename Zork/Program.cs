56ca9b240700dcfd1498e667898188bc72e6262c
ewhite755 <54605007+ewhite755@users.noreply.github.com>
2019-09-19 19:19:27 -0400

    Update Program.cs

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Zork
 {
     class Program
     {
+
+
+       
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
@@ -11,39 +15,91 @@ namespace Zork
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
+                Console.WriteLine(Rooms[PlayerPosition]);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                string response;
+                string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
-                        response = "Thank you for playing!";
+                        outputString = "Thank you for playing!";
                         break;
 
                     case Commands.LOOK:
-                        response = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' lies by the door.";
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' lies by the door.";
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        response = $"You moved {command}.";
+                        bool movedSuccessfully = Move(command);
+                        if (movedSuccessfully)
+                        {
+                            outputString = $"You moved {command}.";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut";
+                        }
                         break;
 
                     default:
-                        response = "Unkown command.";
+                        outputString = "Unkown command.";
                         break;
                 }
 
-                Console.WriteLine(response);
+                Console.WriteLine(outputString);
             }
         }
         
-        private static  Commands ToCommand(string commandString)
+        private static Commands ToCommand(string commandString)
         {
             return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
         }
+        private static bool Move(Commands command)
+        {
+            
+            if (Directions.Contains(command) == false)
+            {
+                throw new ArgumentException();
+            }
+
+            bool movedSuccessfully;
+            switch (command)
+            {
+                case Commands.NORTH:
+                case Commands.SOUTH:
+                    movedSuccessfully = false;
+                    break;
+
+                case Commands.EAST when PlayerPosition < Rooms.Length - 1:
+                    PlayerPosition++;
+                    movedSuccessfully = true;
+                    break;
+
+                case Commands.WEST when PlayerPosition > 0:
+                    PlayerPosition--;
+                    movedSuccessfully = true;
+                    break;
+
+                default:
+                    movedSuccessfully = false;
+                    break;
+            }
+            return movedSuccessfully;
+        }
+
+
+        private static readonly List<Commands> Directions = new List<Commands>()
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST,
+        };
+        private static readonly string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static int PlayerPosition = 1;
     }
 }
