6e68e4d175b1ca5e9a68ea1e79febc678d0c84f1
ewhite755 <54605007+ewhite755@users.noreply.github.com>
2019-09-24 19:28:59 -0400

    Update Program.cs
    2d array

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -5,17 +5,15 @@ namespace Zork
 {
     class Program
     {
-
-
-       
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            SpawnPlayer("West of House");
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
-                Console.WriteLine(Rooms[PlayerPosition]);
+                Console.WriteLine(Rooms[Location.Row, Location.Column]);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
@@ -46,42 +44,38 @@ namespace Zork
                         break;
 
                     default:
-                        outputString = "Unkown command.";
+                        outputString = "Unknown command.";
                         break;
                 }
 
                 Console.WriteLine(outputString);
             }
         }
-        
-        private static Commands ToCommand(string commandString)
-        {
-            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-        }
+
+        private static Commands ToCommand(string commandString) => (Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
+
+
         private static bool Move(Commands command)
         {
-            
-            if (Directions.Contains(command) == false)
-            {
-                throw new ArgumentException();
-            }
 
-            bool movedSuccessfully;
+            bool movedSuccessfully = true;
             switch (command)
             {
-                case Commands.NORTH:
-                case Commands.SOUTH:
-                    movedSuccessfully = false;
+                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
+                    Location.Row++;
                     break;
 
-                case Commands.EAST when PlayerPosition < Rooms.Length - 1:
-                    PlayerPosition++;
-                    movedSuccessfully = true;
+                case Commands.SOUTH when Location.Column > 0:
+                    Location.Row--;
                     break;
 
-                case Commands.WEST when PlayerPosition > 0:
-                    PlayerPosition--;
-                    movedSuccessfully = true;
+
+                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
+                    Location.Column++;
+                    break;
+
+                case Commands.WEST when Location.Column > 0:
+                    Location.Column--;
                     break;
 
                 default:
@@ -91,6 +85,29 @@ namespace Zork
             return movedSuccessfully;
         }
 
+        private static void SpawnPlayer(string roomName)
+        {
+            Location = IndexOf(Rooms, roomName);
+            if ((Location.Row, Location.Column) == (-1,-1))
+            {
+                throw new Exception($"Did not find room: {roomName}");
+            }
+        }
+
+        private static (int Row, int Column) IndexOf(string[,] values, string valueToFind)
+        {
+            for (int row = 0; row < values.GetLength(0); row++)
+            {
+                for (int column = 0; column < values.GetLength(1); column++)
+                {
+                    if (valueToFind == values[row, column])
+                    {
+                        return (row, column);
+                    }
+                }
+            }
+            return (-1, -1);
+        }
 
         private static readonly List<Commands> Directions = new List<Commands>()
         {
@@ -99,7 +116,14 @@ namespace Zork
             Commands.EAST,
             Commands.WEST,
         };
-        private static readonly string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
-        private static int PlayerPosition = 1;
+
+        private static readonly string[,] Rooms = {
+            { "Rocky Trail", "South of House", "Canyon View" },
+            { " Forest", "West of House", "Behind House"},
+            { "Dense Woods", "North of House", "Clearing" }
+        };
+
+        private static (int Row, int Column) Location;
     }
 }
+
